286eda1b5a784a40816e404842af1e341ff7c0ce
Rahul Krishnan M <rahulkrishnan.m2003@gmail.com>
2026-02-07 15:42:04 +0530

    new code

---
+++ cpp/displayname.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    string n;
+    cout << "Please enter your name: ";
+    cin >> n;
+    cout << "Hello! " << n << endl;
+    return 0;
+}

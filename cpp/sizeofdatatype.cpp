c6f51123ccdbd7f6c2e2334d0bc280117e8bffcd
Rahul Krishnan M <rahulkrishnan.m2003@gmail.com>
2026-02-15 19:57:51 +0530

    updated

---
+++ cpp/sizeofdatatype.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int integerType;
+    float floatType;
+    char charType;
+    bool booleanType;
+    double doubleType;
+    long longType;
+    string stringType;
+
+    cout << sizeof ( integerType ) << endl; 
+    cout << sizeof ( floatType ) << endl;
+    cout << sizeof ( charType ) << endl;
+    cout << sizeof ( booleanType ) << endl;
+    cout << sizeof ( doubleType ) << endl;
+    cout << sizeof ( longType ) << endl; 
+    cout << sizeof (stringType ) << endl;
+}

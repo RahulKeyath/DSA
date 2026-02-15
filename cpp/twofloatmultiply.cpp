3fdd7ef1ab13eedc0680ee958cc7e5abbcaa4e5a
Rahul Krishnan M <rahulkrishnan.m2003@gmail.com>
2026-02-15 20:09:19 +0530

    updated

---
+++ cpp/twofloatmultiply.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    float a , b;
+    cout << "Enter values of a and b: ";  
+    cin >> a >> b;
+    cout << "Product of a and b is: " << a * b;
+    return 0;
+}

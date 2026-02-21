20d63035c465008af32c4a4554465dd522428318
Rahul Krishnan M <rahulkrishnan.m2003@gmail.com>
2026-02-21 18:00:24 +0530

    updated

--- cpp/sumofn.cpp
+++ cpp/sumofn.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+
+using namespace std;
+
+int main() {
+    int number, result = 0;
+    cout << "Enter a number: ";
+    cin >> number;
+    for (int i = 1; i <= number; i++){
+        result += i;
+    }
+    cout << "The sum of "<< number << " numbers is: " << result;
+    return 0;
+}

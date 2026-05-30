70f7957f75cd5e919c8cc116454ea45c75fd6ca2
Rahul Krishnan M <rahulkrishnan.m2003@gmail.com>
2026-05-30 16:42:03 +0530

    pattern printing

---
+++ cpp/pattern4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int num;
+    cin >> num;
+
+    int i = 1;
+    while (i <= num){
+        int j = 1;
+        while (j <= i){
+            cout << i << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+}
